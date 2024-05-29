bb2081e7018c3c407dd41ce9a3c77d210ffe27da
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-05-29 19:04:03 +0100

    DO RE MI FA SOL LA SI ! On iOS and Android.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(XylophoneApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('assets_note$soundNumber.wav'));
+  }
+
+  Color colorPicker(int soundNumber) {
+    switch (soundNumber) {
+      case 1:
+        return Colors.red;
+      case 2:
+        return Colors.orange;
+      case 3:
+        return Colors.yellow;
+      case 4:
+        return Colors.green;
+      case 5:
+        return Colors.teal;
+      case 6:
+        return Colors.blue;
+      case 7:
+        return Colors.purple;
+      default:
+        return Colors.black;
+    }
+  }
+
+  Widget buildKey(int soundNumber) {
+    return Expanded(
+      child: MaterialButton(
+        color: colorPicker(soundNumber),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1),
+              buildKey(2),
+              buildKey(3),
+              buildKey(4),
+              buildKey(5),
+              buildKey(6),
+              buildKey(7),
+            ],
+          ),
         ),
       ),
     );
